67772289f6c8e2b7f8b5d5e3cdd4e38704c81398
Kirill V. <scriptythememer@gmail.com>
2024-10-03 23:39:54 +0700

    fun facts

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const CAT_PIC_AMOUNT = 2;
 
+const FUN_FACTS = [
+    "My favorite color is purple. Duh",
+    "I am from Russia.",
+    "Currently I use Fedora Linux as my main OS.",
+    "I do video editing sometimes, most of my time spent editing was on Adobe Premiere Pro (adobe sucks)",
+    "I hate all of my past usernames...",
+    "I like watching small youtuber's under 10k subs.",
+    "My official profession is Слесарь КИПиА (instrumentation technician). It's pretty cool",
+    "Yakuza AKA Like A Dragon is #1 'game series as a whole' ever.",
+    "My first game I played was probably Sonic Adventure: DX.",
+    "I wanna try making a game and uploading it to Steam.",
+    "My breakfast is a butter sandwich + tea combo.",
+    "My favorite food is probably Блины (pancakes)",
+    "I am not good at cooking.",
+    "I started programming as a hobby in 2022. Before that I was just scripting hello world's here and there.",
+    "Lua is my favorite programming language because It's just so silly!!",
+    "I really like eating bread. Just overall everything bread-related.",
+    "I am bad at school and i'm getting bad grades.",
+    "Don't ask me if i'm autistic or smth like that. I don't know lol"
+]
+
 /*
 window.onload = function() {
     var num = Math.floor(Math.random() * CAT_PIC_AMOUNT) + 1;
