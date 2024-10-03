bc8c3f315a44bb0a208fe6aca47f343094b6ad7e
Kirill V. <scriptythememer@gmail.com>
2024-10-03 15:19:58 +0700

    cat of the day

---
+++ index.js
@@ -0,0 +1,11 @@
+const CAT_PIC_AMOUNT = 2;
+
+window.onload = function() {
+    var num = Math.floor(Math.random() * CAT_PIC_AMOUNT) + 1;
+    var filename = "img/cat-" + num + ".jpg";
+
+    var element = document.getElementById("cat");
+    element.src = filename;
+
+    console.log(filename)
+}
